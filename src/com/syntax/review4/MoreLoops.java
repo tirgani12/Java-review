9eff64b685d1cb24de86dd09164d2cf346775b15
tirgani12 <ahmed.abhr6@gmail.com>
2023-01-19 21:10:01 -0500

    review0

---
+++ src/com/syntax/review4/MoreLoops.java
@@ -0,0 +1,14 @@
+package com.syntax.review4;
+
+import java.util.Scanner;
+
+public class MoreLoops {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 3; i++) {
+            for (int j = 1; j <= 3; j++) {
+                System.out.println(i);
+            }
+        }
+    }
+
+}
--- src/com/syntax/review4/NestedLoops.java
+++ src/com/syntax/review4/NestedLoops.java
@@ -10,11 +10,7 @@ public class NestedLoops {
                 System.out.println(j);
             }
         }
-        for (int i=1;i<5;i++){
-            System.out.println("Hello");
-            for (int j=1;j<=2;j++){
-                System.out.println("bye");
-            }
+
         }
     }
 

b38d94ed3df6846fb297a547a0596144e8f2a584
tirgani12 <ahmed.abhr6@gmail.com>
2023-01-26 21:59:29 -0500

    review4

--- src/com/syntax/review4/NestedLoops.java
+++ src/com/syntax/review4/NestedLoops.java
@@ -1,17 +1,2 @@
-package com.syntax.review4
-
-public class NestedLoops {
-    public static void main(String[] args) {
-
-        //nested loop - loop inside another loop
-        for (int i = 1; i <= 3; i++) {//outer loop
-            System.out.println(i);
-            for (int j = 1; j <= 4; j++) {//inner loop
-                System.out.println(j);
-            }
-        }
-
-        }
-    }
-
+package com.syntax.review4;public class NestedLoops {
 }
---
+++ src/com/syntax/review5/TwoDArrayReview.java
@@ -0,0 +1,2 @@
+package com.syntax.review5;public class TwoDArrayReview {
+}

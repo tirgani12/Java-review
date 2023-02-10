c9cdae313578a757e0538b69c7310258cfe18adc
tirgani12 <ahmed.abhr6@gmail.com>
2023-02-09 19:12:07 -0500

    review6

---
+++ src/com/syntax/review6/Flower.java
@@ -0,0 +1,21 @@
+package com.syntax.review6;
+
+public class Flower {
+    //attributes,properties
+    String color;
+    String  nane;
+    int price;
+    //behavior
+    void smell(){
+
+        System.out.println("flower blooms");
+    }
+    void bloom(){
+        System.out.println("flower bloom");
+
+    }
+    void grow(){
+        System.out.println("flower grow");
+
+    }
+}
---
+++ src/com/syntax/review6/Garden.java
@@ -0,0 +1,25 @@
+package com.syntax.review6;
+
+public class Garden {
+    public static void main(String[] args) {
+        Flower flower1=new Flower();
+        flower1.nane="Hibiscus";
+        flower1.color="red";
+        flower1.price=5;
+        //accessing methods of flowers class
+        flower1.bloom();
+        flower1.grow();
+        flower1.smell();
+        System.out.println("creating second object of the flower class");
+
+        Flower flower2=new Flower();
+
+        flower2.nane="rose";
+        flower2.color="blue";
+        flower2.price=5;
+
+String str1=" review b15";
+
+
+    }
+}

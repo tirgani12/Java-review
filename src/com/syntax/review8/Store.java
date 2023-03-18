4a8630841a57e5e7a96ac483c4e91ec4ee30c594
tirgani12 <ahmed.abhr6@gmail.com>
2023-03-17 21:54:17 -0400

    review8

---
+++ src/com/syntax/review8/Store.java
@@ -0,0 +1,27 @@
+package com.syntax.review8;
+
+public class Store {
+    public static void main(String[] args) {
+
+
+
+        Furniture table=new Table();
+        table.comfort(); //runtime polymorphism
+        table.assemble();
+
+        Table t=new Table();
+        t.assemble();
+        t.comfort();
+        t.buildTable("Wood");
+
+        Chair.breaks();
+        Furniture.breaks(); //static method called by using class name
+
+        Furniture chair=new Chair();
+        chair.assemble();
+        chair.comfort(); //runtime polymorphism
+        //proof that method breaks is not overriden
+        chair.breaks(); //since we are not overriding methods breaks will be executed from Parent
+        //this is also not the right way to call static method
+    }
+}

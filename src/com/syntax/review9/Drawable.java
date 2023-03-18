becbe8ec74da35870fc272e7afcadc25c80d653a
tirgani12 <ahmed.abhr6@gmail.com>
2023-03-17 21:52:36 -0400

    review9

---
+++ src/com/syntax/review9/Drawable.java
@@ -0,0 +1,53 @@
+package com.syntax.review9;
+
+public interface Drawable {
+    String TOOL = "Pencil"; //by default it is public static final variable
+
+    void draw(); // by default this method is public abstract
+
+    //above is how Interface used to be before Java 8
+    //after Java 8 static and default methods were added to the Interface
+    default void print() {
+        System.out.println("We are drawing object using " + TOOL);
+    }
+
+    static void erase() {
+        System.out.println("All drawings are erasable");
+    }
+}
+
+abstract class Shape {
+
+    public String color;
+    protected double width, length;
+
+    Shape(String color, double length, double width) {
+        this.color = color;
+        this.length = length;
+        this.width = width;
+    }
+
+    public abstract double calculateArea();
+}
+
+class Rectangle extends Shape implements Drawable {
+
+    Rectangle(String color, double length, double width){
+        super(color, length, width);
+    }
+
+    public double calculateArea(){
+
+        return length*width;
+    }
+    public void draw() {
+        System.out.println("I am drawing rectangle");
+    }
+}
+
+class Square implements Drawable {
+
+    public void draw() {
+        System.out.println("I am drawing Square");
+    }
+}

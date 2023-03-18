48c869ee175e778e92a718bc9d3270adee4febdf
tirgani12 <ahmed.abhr6@gmail.com>
2023-03-17 21:48:32 -0400

    review12

---
+++ src/com/syntax/review12/ThrowKeyword.java
@@ -0,0 +1,15 @@
+package com.syntax.review12;
+
+public class ThrowKeyword {
+    public static void main(String[] args) {
+
+        throwDemo();
+
+
+
+    }
+
+    static void throwDemo(){
+        throw new NullPointerException();
+    }
+}
